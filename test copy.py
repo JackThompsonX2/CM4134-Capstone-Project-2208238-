e7b1964a2e22cbee8198461f12afebc0044a4a2a
JackThompsonX2 <thompsonjack845@gmail.com>
2026-04-05 10:27:46 +0100

    i've added a checkpoint to the model so it should run well

---
+++ test copy.py
@@ -0,0 +1,39 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import librosa
+import cv2
+from PIL import Image
+import tensorflow
+from tensorflow import keras
+import warnings
+warnings.filterwarnings('ignore')
+
+model= keras.models.load_model("CheckPoint.keras")
+img = cv2.imread("Music_separation_dataset/test/mix/4052_mix.png",0)
+
+img=cv2.resize(img,(128,128))
+img= np.reshape(img,(-1, 128, 128, 1))
+img=img/255
+drum = np.array([[0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,0,0,0,0,0,0,0,0,0,0,0]])
+
+predimg= np.squeeze(model.predict([img,drum]))
+
+prediction= predimg*255
+print(prediction.shape)
+
+cv2.imwrite("separated_drum_part.png",prediction)
+
+img = cv2.imread("music_separation_2/train/full_mix/462_full_mix.png",0)
+
+img=cv2.resize(img,(128,128))
+img= np.reshape(img,(-1, 128, 128, 1))
+img=img/255
+acoustic = np.array([[0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0,0,0,0,0,0,0,0,0,0,0]])
+
+predimg= np.squeeze(model.predict([img,acoustic]))
+
+prediction= predimg*255
+print(prediction.shape)
+
+cv2.imwrite("separated_part_acoustic.png",prediction)
