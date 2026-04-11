439f966bb29614da8630311b0258d690cf4e5136
JackThompsonX2 <thompsonjack845@gmail.com>
2026-04-11 16:11:31 +0100

    so tested model for identification can do single well needs to be updated for mixed fully

--- test copy.py
+++ test copy.py
@@ -14,9 +14,9 @@ warnings.filterwarnings('ignore')
 
 
 model= keras.models.load_model("CheckPoint.keras")
-img = cv2.imread("music_separation_2/test/full_mix/837_full_mix.png",0)
+img = cv2.imread("Music_separation_dataset/test/mix/4044_mix.png",0)
 
-img=cv2.resize(img,(128,128))
+img=img[:128,:128]
 img= np.reshape(img,(-1, 128, 128, 1))
 img=img/255
 drum = np.array([[0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,0,0,0,0,0,0,0,0,0,0,0]])
@@ -28,9 +28,9 @@ print(prediction.shape)
 
 cv2.imwrite("separated_drum_partcheck.png",prediction)
 
-img = cv2.imread("music_separation_2/test/full_mix/843_full_mix.png",0)
+img = cv2.imread("Music_separation_dataset/test/mix/4052_mix.png",0)
 
-img=cv2.resize(img,(128,128))
+img=img[:128,:128]
 img= np.reshape(img,(-1, 128, 128, 1))
 img=img/255
 acoustic = np.array([[0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0,0,0,0,0,0,0,0,0,0,0]])
